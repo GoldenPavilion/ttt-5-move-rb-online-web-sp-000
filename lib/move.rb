eba12427fa96963052120a56b00c1a821083efef
Robert Stine <rcorystine@gmail.com>
2020-12-08 22:05:55 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,10 +6,10 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(input)
-  puts input.to_i
+def input_to_index(User_input)
+  returns (User_input.to_i - 1)
 end
 
-def move(board, index, "X")
-  board
+def move(board, index, character = "X")
+  board[index] = character
 end
